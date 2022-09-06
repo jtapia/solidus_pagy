e731348f55b361a6840ad7ec5c2b4bc271279a72
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2022-09-06 10:51:34 -0500

    Load solidus references

--- lib/solidus_pagy.rb
+++ lib/solidus_pagy.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'solidus_core'
+require 'solidus_backend'
+require 'solidus_support'
+require 'deface'
+
 require 'solidus_pagy/configuration'
 require 'solidus_pagy/version'
 require 'solidus_pagy/engine'
